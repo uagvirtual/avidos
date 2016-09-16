b7ea1524cc17eb7dc4875e2f08648244aed98990
Felix Olivares <felixoe@gmail.com>
2016-09-15 22:45:04 -0600

    Created custom cell for Routes section. Connected to services, created Routes object and linked to the table view. Displayed results retrieved from the service.

---
+++ PuntoJalisco/PJRouteTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PJRouteTableViewCell.swift
+//  PuntoJalisco
+//
+//  Created by Félix Olivares on 9/15/16.
+//  Copyright © 2016 Felix. All rights reserved.
+//
+
+import UIKit
+
+class PJRouteTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var routeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- PuntoJalisco/PJRoutsViewController.swift
+++ PuntoJalisco/PJRoutsViewController.swift
@@ -9,18 +9,26 @@
 import UIKit
 import Alamofire
 
-class PJRoutsViewController: UIViewController {
+class PJRoutsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     let locationPath = Constants.Paths.mainPath + Constants.Paths.locationPath
+    let cellBackgroundColor = UIColor(red: 117.0/255.0, green: 0, blue: 27.0/255.0, alpha: 1)
     
     var params:[String:String] = [Constants.Location.routeKey:"368", Constants.Location.latKey:"20.6716283647243", Constants.Location.lonKey:"-103.368670"]
     var parsedResponse = ""
-    var routes:[PJRouteObject] = []
+    var routesArray:[PJRouteObject] = []
+    let kRouteCellIdentifier = "RouteCellIdentifier"
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         getLocation()
+        
+        let routeCell = UINib(nibName: "PJRouteTableViewCell", bundle: nil)
+        tableView.registerNib(routeCell, forCellReuseIdentifier: kRouteCellIdentifier)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,10 +47,32 @@ class PJRoutsViewController: UIViewController {
                 let parsedRoutes = JSONParser().parseJSON(response.result.debugDescription)
                 for eachRoute in parsedRoutes{
                     let route = PJRouteObject.init(route: eachRoute as! [String : String])
-                    self.routes.append(route)                    
+                    self.routesArray.append(route)
                 }
-                print(self.routes)
+                print(self.routesArray)
+                self.tableView.reloadData()
                 
         }
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return routesArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:PJRouteTableViewCell = tableView.dequeueReusableCellWithIdentifier(kRouteCellIdentifier) as! PJRouteTableViewCell
+        cell.routeLabel.text = "Ruta " + routesArray[indexPath.row].routeNumber!
+        cell.accessoryType = .DisclosureIndicator
+//        cell.accessoryView?.backgroundColor = UIColor.clearColor()
+        cell.contentView
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.backgroundColor = cellBackgroundColor
     }
 }
